14ad2a914d5d3f17944e273c817c8b0a214c3609
GabeMed <gabrymedeiros7@gmail.com>
2025-05-21 12:43:16 -0300

    Defining the initial navegation

--- frontend/finance-app/src/routes.tsx
+++ frontend/finance-app/src/routes.tsx
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import RegistrationPage from "./pages/RegistrationPage";
 
-const router = createBrowserRouter([{ path: "/", element: <LoginPage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <LoginPage /> },
+  { path: "/home", element: <HomePage /> },
+  { path: "/register", element: <RegistrationPage /> },
+]);
 
 export default router;
