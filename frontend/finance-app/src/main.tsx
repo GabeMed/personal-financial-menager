2871f0857f689c131fd8652dfa44d5990f66b4e3
GabeMed <gabrymedeiros7@gmail.com>
2025-05-21 12:46:56 -0300

    Setting up the providers

--- frontend/finance-app/src/main.tsx
+++ frontend/finance-app/src/main.tsx
@@ -3,11 +3,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
