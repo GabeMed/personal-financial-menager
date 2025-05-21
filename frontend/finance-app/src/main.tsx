bafbe6a489b2bd2f30121a3d2a3cf534525e673d
GabeMed <gabrymedeiros7@gmail.com>
2025-05-21 00:53:06 -0300

    Setting up routes

--- frontend/finance-app/src/main.tsx
+++ frontend/finance-app/src/main.tsx
@@ -1,13 +1,13 @@
 import { Provider } from "./components/ui/provider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
