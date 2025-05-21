379ea9ed0d0bd817d0dff639ac123d0a452234a3
GabeMed <gabrymedeiros7@gmail.com>
2025-05-21 19:36:07 -0300

    Updating schemas

---
+++ backend/app/schemas/transaction.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from decimal import Decimal
+from pydantic import BaseModel
+from backend.app.schemas.category import CategoryResponse
+from typing import Literal
+
+
+class TransactionBase(BaseModel):
+    type: Literal["expense", "income"]
+    amount: Decimal
+    description: str | None = None
+    date: datetime
+    category_id: int
+
+
+class TransactionCreate(TransactionBase):
+    pass
+
+
+class TransactionUpdate(BaseModel):
+    type: str | None = None
+    amount: Decimal | None = None
+    description: str | None = None
+    date: datetime | None = None
+    category_id: int | None = None
+
+
+class TransactionResponse(TransactionBase):
+    id: int
+    amount: Decimal
+    category: CategoryResponse
+
+    class Config:
+        orm_mode = True
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,16 +1,21 @@
-from pydantic import BaseModel
+from decimal import Decimal
+from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
     username: str
-    email: str
+    email: EmailStr
     password: str
 
 
 class UserResponse(BaseModel):
     id: int
     username: str
-    email: str
+    email: EmailStr
+    balance: Decimal
 
     class Config:
         orm_mode = True
+
+
+##TODO add user update, just like I did with the other entities
