b695a94483501a8054996a50f48d2ea214b02c74
GabeMed <gabrymedeiros7@gmail.com>
2025-05-22 00:18:36 -0300

    Adding react-query-devtools

--- frontend/finance-app/src/main.tsx
+++ frontend/finance-app/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <Provider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </Provider>
   </StrictMode>
