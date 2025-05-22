19c1c94dabab287a3d66ce7593ccc71a9b90b24b
GabeMed <gabrymedeiros7@gmail.com>
2025-05-22 18:10:24 -0300

    Stop refetching everytime

--- frontend/finance-app/src/main.tsx
+++ frontend/finance-app/src/main.tsx
@@ -7,7 +7,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "@/context/AuthContext.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
