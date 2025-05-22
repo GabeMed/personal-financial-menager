a36b2e077a4e09c133b5e7c85c3be04ae1779908
GabeMed <gabrymedeiros7@gmail.com>
2025-05-22 18:56:31 -0300

    Backend attribute name missmatch

--- frontend/finance-app/src/schemas/transaction.ts
+++ frontend/finance-app/src/schemas/transaction.ts
@@ -5,10 +5,10 @@ export const NewTransactionSchema = z
     description: z.string().min(1, "Must have a description"),
     amount: z.number().positive("Must be a positive number"),
     type: z.enum(["income", "expense"]),
-    categoryId: z.number().int().nullable(),
+    category_id: z.number().int().nullable(),
     newCategoryName: z.string().optional(),
   })
-  .refine((v) => v.categoryId !== null || v.newCategoryName?.length, {
+  .refine((v) => v.category_id !== null || v.newCategoryName?.length, {
     message: "Select a category or create a new one",
   });
 export type NewTransactionDTO = z.infer<typeof NewTransactionSchema>;
